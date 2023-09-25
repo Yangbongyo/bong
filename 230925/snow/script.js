57cb95969b5d254b68786d8c1d35aedb2aa9109e
Yangbongyo <comnuly@naver.com>
2023-09-25 13:55:00 +0900

    눈내리기

---
+++ 230925/snow/script.js
@@ -0,0 +1,31 @@
+/**
+ * wrapper에가 snow를 추가해서 animation
+ */
+
+const wrapper = document.querySelector(".wrapper")
+console.log(wrapper)
+
+const randomGenerator = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
+
+for (let i = 0; i < 100; i++) {
+    const snow = document.createElement('span')
+    snow.classList.add('snow')
+    snow.style.left = `${randomGenerator(0, 100)}%`
+    snow.style.animationDuration = `${randomGenerator(10, 30)}s`
+    snow.style.animationDelay = `${randomGenerator(0, 100)}ms`
+    snow.style.opacity = Math.random();
+    // size random
+    let size = randomGenerator(3, 10)
+    snow.style.width = `${size}px`
+    snow.style.height = `${size}px`
+    snow.style.filter = `blur${randomGenerator(1, 3)}px`
+    // wrapper 안에 snow 클래스를 추가한다
+    wrapper.append(snow)
+    console.log(snow)
+    // setTimeout(() => {
+
+    // })
+}
