5acb8a46b0e609506f9adc5702f92cd6b012a6e3
Yangbongyo <comnuly@naver.com>
2023-09-26 12:27:59 +0900

    counter

---
+++ 230926/counter/script.js
@@ -0,0 +1,22 @@
+
+const dataList = document.querySelectorAll('[data-counter')
+
+console.log(dataList)
+
+// 반목문을 이용해서 dataList안에 있는 data-counter 값을 출력
+// html : data-counter , js : element.dataset.counter
+for (let i = 0; i < dataList.length; i++) {
+    const target = dataList[i].dataset.counter
+    const timerText = dataList[i].querySelector('.timer')
+    // target / 200 : 소숫점 발생 --- Math.floor (소숫점 제거)
+    const step = Math.floor(target / 200);   // 증감의 형태를 변경해 주는 역할
+    let index = 0;
+    if(timerText){
+    setInterval(() => {
+        if (index < target) {
+            index = index + step
+            timerText.textContent = index
+        }
+    }, 100)
+}
+}  
---
+++ 230926/tab/script.js
@@ -0,0 +1,34 @@
+console.log('test')
+
+const tabList = document.querySelector('.tab-list')
+const tabButton = document.querySelectorAll('.tab-button')
+const selected = document.querySelector('.selected')
+
+const tabItem = document.querySelectorAll('.tab-item')
+const show = document.querySelector('.show')
+
+/*
+for(let i = 0; i < tabButton.length; i++){
+    tabButton[i].classList.remove('selected')
+    tabButton[i].addEventListener("click", () => {
+        for(let j = 0; j < tabButton.length; j++){
+            tabButton[j].classList.remove('selected')
+            tabItem[j].classList.remove('show')
+        }
+        tabButton[i].classList.add('selected') 
+        tabItem[i].classList.add('show')
+         
+    })  
+}
+*/
+
+tabButton.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        tabButton.forEach((item, idx) => {
+            button.classList.remove('selected')   // 자식요소는 index명을 부여하지 않아도 인지해서 적용됨
+            tabItem[idx].classList.remove('show')  // 외부 속성이므로 index명을 명확히 지정해 주어야 한다
+        })
+        button.classList.add('selected')
+        tabItem[idx].classList.add('show')
+    })
+})
